0cf45c470f177029d10ae2f466e465c5da38d7a6
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-07-15 11:30:38 -0300

    teste1

--- cadastro-de-servicos/src/app/components/NavigationBar/index.tsx
+++ cadastro-de-servicos/src/app/components/NavigationBar/index.tsx
@@ -4,26 +4,27 @@ import { NavLink } from "react-router-dom";
 import { Container } from "./styles";
 
 const Header = () => {
-  return (
-    <Container>
-      <NavLink to="/banner">Banner</NavLink>
-      <br />
-      <NavLink to="/sobre-o-servico">Sobre o Serviço</NavLink>
-      <br />
-      <NavLink to="/para-quem-e-o-servico">Para quem é o Servico</NavLink>
-      <br />
-      <NavLink to="/como-funciona">Como funciona</NavLink>
-      <br />
-      <NavLink to="/produtos">Produtos</NavLink>
-      <br />
-      <NavLink to="/diferenciais">Diferenciais</NavLink>
-      <br />
-      <NavLink to="/mais-campos">Mais campos</NavLink>
-      <br />
-      <NavLink to="/faq">FAQ</NavLink>
-      <h1>Teste commit</h1>
-    </Container>
-  );
+	return (
+		<Container>
+			<h1>teste1</h1>
+			<NavLink to="/banner">Banner</NavLink>
+			<br />
+			<NavLink to="/sobre-o-servico">Sobre o Serviço</NavLink>
+			<br />
+			<NavLink to="/para-quem-e-o-servico">Para quem é o Servico</NavLink>
+			<br />
+			<NavLink to="/como-funciona">Como funciona</NavLink>
+			<br />
+			<NavLink to="/produtos">Produtos</NavLink>
+			<br />
+			<NavLink to="/diferenciais">Diferenciais</NavLink>
+			<br />
+			<NavLink to="/mais-campos">Mais campos</NavLink>
+			<br />
+			<NavLink to="/faq">FAQ</NavLink>
+			<h1>Teste commit</h1>
+		</Container>
+	);
 };
 
 export default Header;
