c7341f5d3f6b0f4e82e258cd765068734fa027bd
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-07-15 11:53:46 -0300

    feito

--- cadastro-de-servicos/src/app/components/NavigationBar/index.tsx
+++ cadastro-de-servicos/src/app/components/NavigationBar/index.tsx
@@ -6,8 +6,6 @@ import { Container } from "./styles";
 const Header = () => {
 	return (
 		<Container>
-      <h1>teste</h1>
-      <h2>teste2</h2>
 			<NavLink to="/banner">Banner</NavLink>
 			<br />
 			<NavLink to="/sobre-o-servico">Sobre o Serviço</NavLink>
